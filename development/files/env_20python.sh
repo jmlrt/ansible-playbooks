777a13c2dc6556d9de580d528dd3a330da11289f
jmlrt <git@mailleret.fr>
2016-04-20 08:09:45 +0200

    use python3 virtualenv by default

--- development/files/env_20python.sh
+++ development/files/env_20python.sh
@@ -2,4 +2,7 @@
 # Python environment file
 
 # Activate python virtualenv
-source ~/.apps/venv/bin/activate
+source ~/.apps/venv3/bin/activate
+
+# Keep downloaded packages in cache
+export PIP_DOWNLOAD_CACHE='~/.pip/cache';
